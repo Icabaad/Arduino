1d254087b813c510f56434505eb6367e6487c567
TheDanger <github@fallenreich.com>
2013-10-07 23:46:28 +1100

    Cleanup
    Testing Github. Marvelous Version Control!!

--- PowerFeeder2/PowerFeeder2.ino
+++ PowerFeeder2/PowerFeeder2.ino
@@ -1,4 +1,5 @@
-#include <EmonLib.h>
+// Reads data from Current Cost CC128 Power Meter and feeds it to Arduino Mega which collates with other sensors 
+// and uploads to Xively and SQL
 
 #include <SoftwareSerial.h>
 #define rxPin 3
@@ -7,46 +8,35 @@ SoftwareSerial currentcost(rxPin, txPin);
 String readString, temp, sensor, watts;
 const unsigned int MAX_INPUT = 164;
 int wattsInt;
-int totalPower;
-int hydro;
+long int totalPower;
+long int totalHydro = 0;
 int sensorInt;
-  static char input_line [MAX_INPUT];
-  static unsigned int input_pos = 0;
+int LP;
+static char input_line [MAX_INPUT];
+static unsigned int input_pos = 0;
 int hydroCount = 0; //hydro counter
-int powerCount = 0; //power counter
-int lastTotal = 0; //Last Hydro result
+int totalCount = 0; //power counter
+int lastHydro = 0; //Last Hydro result
 int lastTotal = 0; //Last total power result
 void setup() {
   Serial.begin(9600);
-
   currentcost.begin(57600);
 }
 
-
-
 void loop() {
-
-  //   delay(500);
-
 
   static char input_line [MAX_INPUT];
   static unsigned int input_pos = 0;
 
   if (currentcost.available()) {
-    // delay(1);
-    //***********************    
     //  Serial.print((char)Serial1.read());
     {
       char inByte = currentcost.read ();
-
-      switch (inByte)
-      {
+      switch (inByte) {
       case '\n':   // end of text
         input_line [input_pos] = 0;  // terminating null byte
-
         // terminator reached! process input_line here ...
         process_data (input_line);
-
         // reset buffer for next time
         input_pos = 0;  
         break;
@@ -58,7 +48,7 @@ void loop() {
         // keep adding if not full ... allow for terminating null byte
         if (input_pos < (MAX_INPUT - 1))
           input_line [input_pos++] = inByte;
-   //     Serial.print(inByte);
+        //     Serial.print(inByte);
         break;
 
       }  // end of switch
@@ -67,15 +57,44 @@ void loop() {
 
   }
   //********************Experimental Send Recieve code**************************
-  
-  if(Serial.available() {
-  char ch = Serial.read();
-  switch(ch) {
+
+  if(Serial.available()) {
+    char ch = Serial.read();
+    switch(ch) {
     case 'S':
-      Serial.print(totalPower,DEC); Serial.print(",");  
-  Serial.print(hydro,DEC); Serial.print(",");
-  Serial.print(LP,DEC);Serial.print(",");Serial.print("F");
-    */
+      Serial.print("Received Send Request");
+      totalPower = totalPower / totalCount;
+      if(totalPower < 1) {
+        totalPower = 0;
+      }
+      totalHydro = totalHydro / hydroCount;
+      if(totalHydro < 5) {
+        totalHydro = 0;
+      }
+
+      LP = totalPower - totalHydro;
+      Serial.print(totalPower,DEC); 
+      Serial.print(",");  
+      Serial.print(totalHydro,DEC); 
+      Serial.print(",");
+      Serial.print(LP,DEC);
+      Serial.print(",");
+      Serial.println("F");
+      ch = 0;
+      totalCount = 0;
+      totalPower = 0;
+      totalHydro = 0;
+      hydroCount = 0;
+      Serial.flush();
+      break;
+    default:
+      Serial.print(ch);  
+      Serial.println(" was recieved....WTF do I do with this?! ");
+      Serial.flush();
+      break;
+    }
+  }
+
 }
 
 void process_data (char * data)
@@ -90,35 +109,39 @@ void process_data (char * data)
   sensorInt = sensor.toInt();
 
   if(sensorInt == 0) {
-    lastPower = wattsInt
-    totalPower + lastpower;
-    totalCount ++;
-    Serial.print("Total Count:");Serial.println (totalCount);
+    lastTotal = wattsInt;
+    if(lastTotal > 10) {
+      totalPower = totalPower + lastTotal;
+      totalCount ++;
+      //    Serial.print("Last POwer:");Serial.println (lastTotal);
+      //       Serial.print("Total Count:");Serial.println (totalCount);
+      //      Serial.print("Total Power:");Serial.println (totalPower);
+    }
   }
   else if(sensorInt = 1) {
-    lastHydro = wattsInt
-    hydro + wattsInt;
-    hydroCount ++;
-    Serial.print("Hydro Count:");Serial.println (hydroCount);
+    lastHydro = wattsInt;
+    if(lastHydro > 10) {
+      totalHydro = totalHydro + lastHydro;
+      hydroCount ++;
+      //          Serial.print("Last hydro:");Serial.println (lastHydro);
+      //        Serial.print("Hydro Count:");Serial.println (hydroCount);
+      //         Serial.print("Hydro amount:");Serial.println (totalHydro);
+    }
   }
-  int LP =totalPower - hydro;
-  
-  if (hydro < 0) {
-    hydro = 0;
-  }
-  if (totalPower < 0) {
-    totalPower = 0;
-  }
- // Serial.print("totalpower: ");
- 
- 
- Serial.print("S");
-  Serial.print(totalPower,DEC); Serial.print(",");  
-  Serial.print(hydro,DEC); Serial.print(",");
-  Serial.print(LP,DEC);Serial.print(",");Serial.print("F");
-  Serial.println();  
- // Serial.print("LP: ");
-// Serial.println(LP);
-  // datastreams[8].setInt(LP);
+  //int LP = totalPower - totalHydro;
+  /* 
+   if (totalHydro < 0) {
+   totalHydro = 0;
+   }
+   if (totalPower < 0) {
+   totalPower = 0;
+   }
+   */
 }
+
+
+
+
+
+
 
