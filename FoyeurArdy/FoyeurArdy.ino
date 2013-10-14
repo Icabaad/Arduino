f278c7b58ceae2c52003699e8ca65d124bc86e83
Icabaad <github@fallenreich.com>
2013-10-15 09:05:00 +1100

    Update FoyeurArdy.ino
    Added tmp calc function to be tested
    Added float strings in for Serial Sending
    Added Serial output for Serial Sending

--- FoyeurArdy/FoyeurArdy.ino
+++ FoyeurArdy/FoyeurArdy.ino
@@ -1,4 +1,4 @@
-//parts from   by Bill Earl for Adafruit Industries
+//Adafruit referenced
 
 #include <DHT.h>
 
@@ -7,7 +7,7 @@
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 DHT dht(DHTPIN, DHTTYPE);
 
-int lightPin = A2;    // select the input pin for the potentiometer
+int lightPin = A2;    // Light Pin
 int hotPin = A0; //Hot Out
 int coldPin = A1; //Cold In
 
@@ -18,7 +18,6 @@ float logRange = 5.0; // 3.3v = 10^5 lux
 void setup() {
   analogReference(EXTERNAL); //
   Serial.begin(9600);
-  Serial.println("Adafruit Analog Light Sensor Test");
 }
 
 void loop() {
@@ -28,38 +27,60 @@ void loop() {
   Serial.print("Raw = ");
   Serial.print(rawValue);
   Serial.print(" - Lux = ");
-  Serial.println(RawToLux(rawValue)); 
+  float lightValue = (RawToLux(rawValue))
+  Serial.println(lightValue); 
 
   //read tmp
-  float hotTMP = analogRead(hotPin) * 3.3; 
+  //float hotTMP = analogRead(hotPin) * 3.3; 
+  
+  float tmp36 = analogRead(hotPin) * 3.3 / 1024;
+  
   //  float voltage = reading * 3.3;
-  hotTMP /= 1024.0; 
-  float temperatureH = (hotTMP - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
-  //to degrees ((volatge - 500mV) times 100)
-  Serial.print(temperatureH); 
+  //hotTMP /= 1024.0; 
+  //float temperatureH = (hotTMP - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+  //to degrees ((voltage - 500mV) times 100)
+  float hotTMP = RawToTmp(tmp36)
+  Serial.print(hotTMP); 
   Serial.println(" degrees C");
 
-  float coldTMP = analogRead(coldPin) * 3.3; 
+  float tmp36 = analogRead(coldPin) * 3.3; 
+  
   //  float voltage = reading * 3.3;
-  coldTMP /= 1024.0; 
-  float temperatureC = (coldTMP - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
-  //to degrees ((volatge - 500mV) times 100)
-  Serial.print(temperatureC); 
+ // coldTMP /= 1024.0; 
+  //float temperatureC = (coldTMP - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+  //to degrees ((voltage - 500mV) times 100)
+  float coldTMP = RawToTmp(tmp36)
+  Serial.print(coldTMP);
   Serial.println(" degrees C");
 
   Serial.print("Humidity: "); 
-  Serial.println(dht.readHumidity());
+  float humidity = (dht.readHumidity())
+  Serial.println(humidity);
 
   Serial.print("Humid Temperature = ");
-  Serial.println(dht.readTemperature());
+  float dhTemp = (dht.readTemperature())
+  Serial.println(dhTemp());
 
-  delay(10000);
+Serial.print("S");
+Serial.print(lightValue);Serial.print(",");
+Serial.print(hotTMP);Serial.print(",");
+Serial.print(coldTMP);Serial.print(",");
+Serial.print(humidity);Serial.print(",");
+Serial.print(dhTemp);Serial.print(",");
+  delay(5000);
+  
 }
 
-float RawToLux(int raw)
-{
+float RawToLux(int raw) {
   float logLux = raw * logRange / rawRange;
   return pow(10, logLux);
 }
 
-
+float RawTotmp(float tmp36) {
+  //float tmp = analogRead(hotPin) * 3.3 / 1024; 
+  //  float voltage = reading * 3.3;
+  //hotTMP /= 1024.0; 
+ float tmp = (tmp - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+  //to degrees ((voltage - 500mV) times 100)
+return tmp
+}
