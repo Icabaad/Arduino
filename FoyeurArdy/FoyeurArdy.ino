4acfa58f0ebcbde79c632042d1c1b7f4f1782484
TheDanger <github@fallenreich.com>
2013-10-26 17:51:08 +1100

    Update
    adding xbee broadcast and software serial

--- FoyeurArdy/FoyeurArdy.ino
+++ FoyeurArdy/FoyeurArdy.ino
@@ -1,11 +1,15 @@
 //Adafruit referenced
 //Awesome examples from http://www.desert-home.com/ used for XBee components <3
-
-#include <xbee.h>
+#include <SoftwareSerial.h>
+#include <XBee.h>
 #include <DHT.h>
 
+#define rxPin 3
+#define txPin 4 // again a non existing pin, compiler is OK with this...
+SoftwareSerial SXbee(rxPin, txPin);
+
 //XBEE
-XBee xbee = Xbee();
+XBee xbee = XBee();
 // This is the XBee broadcast address.  You can use the address
 // of any device you have also.
 XBeeAddress64 Broadcast = XBeeAddress64(0x00000000, 0x0000ffff);
@@ -28,6 +32,8 @@ float logRange = 5.0; // 3.3v = 10^5 lux
 void setup() {
   analogReference(EXTERNAL); //
   Serial.begin(9600);
+    SXbee.begin(9600);
+    xbee.setSerial(SXbee);
 }
 
 void loop() {
@@ -83,7 +89,7 @@ void loop() {
   Serial.print(",");
   Serial.print(dhTemp);
   Serial.println(",");
-*/
+
   ZBTxRequest zbtx = ZBTxRequest(Broadcast, (uint8_t *)Hello, strlen(Hello));
   xbee.send(zbtx);
   delay(30000);
@@ -91,7 +97,7 @@ void loop() {
   zbtx = ZBTxRequest(Broadcast, (uint8_t *)Buffer, strlen(Buffer));
   xbee.send(zbtx);
   delay(30000);
-  /*
+  
 
 }
 //********************************eND OF lOOP**********************************
